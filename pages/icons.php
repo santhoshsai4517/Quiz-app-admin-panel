b61e1ea71bfc00400d92eaccd8965459a2247f85
santhoshsai4517 <santhoshsai4517@gmail.com>
2021-07-05 11:11:40 +0530

    Converted html files to php files

--- pages/icons.php
+++ pages/icons.php
@@ -35,7 +35,7 @@
             <!-- Navigation -->
             <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">Startmin</a>
+                    <a class="navbar-brand" href="index.php">Startmin</a>
                 </div>
 
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -114,7 +114,7 @@
                             <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
                             </li>
                             <li class="divider"></li>
-                            <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                            <li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                             </li>
                         </ul>
                     </li>
@@ -136,46 +136,46 @@
                                 <!-- /input-group -->
                             </li>
                             <li>
-                                <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                                <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                             </li>
                             <li>
                                 <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
                                     <li>
-                                        <a href="flot.html">Flot Charts</a>
+                                        <a href="flot.php">Flot Charts</a>
                                     </li>
                                     <li>
-                                        <a href="morris.html">Morris.js Charts</a>
+                                        <a href="morris.php">Morris.js Charts</a>
                                     </li>
                                 </ul>
                                 <!-- /.nav-second-level -->
                             </li>
                             <li>
-                                <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                                <a href="tables.php"><i class="fa fa-table fa-fw"></i> Tables</a>
                             </li>
                             <li>
-                                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                                <a href="forms.php"><i class="fa fa-edit fa-fw"></i> Forms</a>
                             </li>
                             <li class="active">
                                 <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
                                     <li>
-                                        <a href="panels-wells.html">Panels and Wells</a>
+                                        <a href="panels-wells.php">Panels and Wells</a>
                                     </li>
                                     <li>
-                                        <a href="buttons.html">Buttons</a>
+                                        <a href="buttons.php">Buttons</a>
                                     </li>
                                     <li>
-                                        <a href="notifications.html">Notifications</a>
+                                        <a href="notifications.php">Notifications</a>
                                     </li>
                                     <li>
-                                        <a href="typography.html">Typography</a>
+                                        <a href="typography.php">Typography</a>
                                     </li>
                                     <li>
-                                        <a href="icons.html" class="active"> Icons</a>
+                                        <a href="icons.php" class="active"> Icons</a>
                                     </li>
                                     <li>
-                                        <a href="grid.html">Grid</a>
+                                        <a href="grid.php">Grid</a>
                                     </li>
                                 </ul>
                                 <!-- /.nav-second-level -->
@@ -214,10 +214,10 @@
                                 <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
                                     <li>
-                                        <a href="blank.html">Blank Page</a>
+                                        <a href="blank.php">Blank Page</a>
                                     </li>
                                     <li>
-                                        <a href="login.html">Login Page</a>
+                                        <a href="login.php">Login Page</a>
                                     </li>
                                 </ul>
                                 <!-- /.nav-second-level -->
